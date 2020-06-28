f0e10e3275818555adf1c5693ca30e31a4423d5c
lingfeng.hu <137714477@qq.com>
2020-06-28 15:38:03 +0800

    add

--- src/main/java/com/sf/test/App.java
+++ src/main/java/com/sf/test/App.java
@@ -1,20 +1,12 @@
 package com.sf.test;
 
-import com.sf.core.annotation.AutoWired;
-import com.sf.core.annotation.fun.Fun;
 import com.sf.core.app.Application;
-import com.sf.test.m1.Test;
 
 public class App{
 
-    @AutoWired
-    private Test test;
     public static void main(String[] args) throws Exception {
         Application.run(App.class,args);
         System.out.println();
     }
-    @Fun
-    public void t(){
-        System.out.println(test.str);
-    }
+
 }
