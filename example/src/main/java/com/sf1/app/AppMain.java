33e06aab3e683f843a373163cae77cb4860e4610
feng-t <137714477@qq.com>
2020-10-23 18:42:32 +0800

    update

--- example/src/main/java/com/sf1/app/AppMain.java
+++ example/src/main/java/com/sf1/app/AppMain.java
@@ -1,9 +1,12 @@
 package com.sf1.app;
 
+import com.sf.app.Application;
+
+@Ann
 public class AppMain {
-//    public static void main(String[] args){
-//        Application.run(AppMain.class,args);
-//    }
+    public static void main(String[] args){
+        Application.run(AppMain.class,args);
+    }
 
     @Override
     public String toString() {
