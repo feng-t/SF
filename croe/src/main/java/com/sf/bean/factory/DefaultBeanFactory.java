71670c28fc98f442e3a260b359a375b92864060a
feng-t <137714477@qq.com>
2020-12-04 17:37:10 +0800

    update

--- croe/src/main/java/com/sf/bean/factory/DefaultBeanFactory.java
+++ croe/src/main/java/com/sf/bean/factory/DefaultBeanFactory.java
@@ -1,24 +1,15 @@
 package com.sf.bean.factory;
 
-import com.sf.annotation.Bean;
 import com.sf.annotation.Component;
-import com.sf.annotation.EnableAOP;
 import com.sf.bean.Resource;
 import com.sun.xml.internal.ws.org.objectweb.asm.ClassAdapter;
+import com.sun.xml.internal.ws.org.objectweb.asm.MethodVisitor;
 
-import java.lang.annotation.Annotation;
-import java.lang.annotation.Target;
-import java.lang.reflect.AnnotatedType;
-import java.util.*;
+import java.util.Set;
 
 public class DefaultBeanFactory extends ParentBeanFactory{
-    private final Map<Class<Annotation>,List<Class<?>>> annotationMap=new HashMap<> ();
     public DefaultBeanFactory(Set<Resource> resourceSet) throws ClassNotFoundException {
         super(resourceSet);
-        List<Resource> filter = getFilter(Component.class);
-        for (Resource resource : filter) {
-            annotationMap.getOrDefault(Component.class,new ArrayList<>()).add(resource.getBeanClass());
-        }
     }
 
 
@@ -32,16 +23,6 @@ public class DefaultBeanFactory extends ParentBeanFactory{
     @Override
     public Object loadBean(Resource resource) throws Exception {
         return super.loadBean(resource);
-    }
-
-    public List<Resource> getFilter(Class<? extends Annotation> annotation) throws ClassNotFoundException {
-        List<Resource> resources = new ArrayList<>();
-        for (Resource resource : this.resources) {
-            if(findAnnotation.isContains(resource.getBeanClass(),annotation)){
-                resources.add(resource);
-            }
-        }
-        return resources;
     }
 
 
