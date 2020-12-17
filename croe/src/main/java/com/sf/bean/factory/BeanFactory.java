5e9b39df231d1a94269d5353e20ee85ff5c643fd
feng-t <137714477@qq.com>
2020-12-17 19:05:58 +0800

    update

--- croe/src/main/java/com/sf/bean/factory/BeanFactory.java
+++ croe/src/main/java/com/sf/bean/factory/BeanFactory.java
@@ -4,6 +4,7 @@ import com.sf.bean.Resource;
 
 import java.lang.annotation.Annotation;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -18,41 +19,6 @@ public class BeanFactory {
 
 
 
-    /**
-     * 查看class是否存在注解，只针对类上面的注解
-     * @param c
-     * @param target
-     * @return
-     */
-    public boolean findAnnotation(Class<?> c, Class<? extends Annotation> target){
-        HashSet<Class<? extends Annotation>> set = new HashSet<>();
-        boolean present = c.isAnnotationPresent(target);
-        if (!present){
-            Annotation[] annotations = c.getDeclaredAnnotations();
-            for (Annotation annotation : annotations) {
-                if (findAnnotation(set,annotation.annotationType(),target)){
-                    return true;
-                }
-            }
-        }
-        return present;
-    }
-    private boolean findAnnotation(Set<Class<? extends Annotation>> set, Class<? extends Annotation> var, Class<? extends Annotation> target){
-        if (var==target){
-            return true;
-        }
-        Annotation[] annotations = var.getDeclaredAnnotations();
-        for (Annotation annotation : annotations) {
-            Class<? extends Annotation> type = annotation.annotationType();
-            if (type==target){
-                return true;
-            }else if (!set.contains(type)){
-                set.add(type);
-                if (findAnnotation(set,type,target)){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+
+
 }
